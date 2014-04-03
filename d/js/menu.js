58e4e48ab4f42b35cd376a8aca90e48bd8d51303
Timothy Comeau <timothy@timothycomeau.com>
2014-04-03 12:18:34 -0400

    inital commit

---
+++ d/js/menu.js
@@ -0,0 +1,37 @@
+// parses the url into an array variable named pathArray
+var pathArray = window.location.pathname.split('/');
+
+// in this case url is localhost:8888/dev/js-url/x
+// where x = a menu item like art, other, about
+// array[0] = localhost:8888
+// array[1] = dev
+// array[2] = js-url
+// array[3] = x
+
+
+// if path = x then underline menu item
+// menu items are <li id="a"> etc
+// getElementByID and rewrite innerHTML with underline tags
+
+//we use document.write to show it is reading correctly and should be deleted
+
+
+if (pathArray[3] == 'about') 
+	{
+	document.write("path is about");
+	document.getElementById('a').innerHTML = '<u>About</u>';
+	}
+	if
+	 (pathArray[3] == "web") 
+	{
+ 	document.write("path is web");
+ 	document.getElementById('b').innerHTML = '<u>Web</u>';
+	 }
+	 if (pathArray[3] == "art") {
+ 	document.write("path is art");
+ 	document.getElementById('c').innerHTML = '<u>Art</u>';
+	 };
+	 if (pathArray[3] == "other") {
+ 	document.write("path is other");
+ 	document.getElementById('d').innerHTML = '<u>Other</u>';
+	 };
---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Title</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="d/css/style.css">
+</head>
+<body>
+<!--[if lt IE 7]><p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p><![endif]-->
+
+<nav>
+	<ul>
+		<li id='a'><a href="about">About</a></li>
+		<li id='b'><a href="web">Web</a></li>
+		<li id='c'><a href="art">Art</a></li>
+		<li id='d'><a href="other">Other</a></li>
+	</ul>
+</nav>
+
+<?php switch ( $_GET['page'] ) {
+
+case 'about':
+include ('one.php') ;
+break;
+
+case 'web':
+include ('two.php') ;
+break;
+
+case 'art':
+include ('three.php');
+break;
+
+case 'other':
+include ('four.php');
+break;
+
+}
+?>
+
+<br><br><br>
+<!--
+<script>
+	var url = window.location.href;
+	var pathArray = window.location.pathname.split('/');
+	document.write(url);
+</script>
+<br><br>
+<script>
+	document.write(pathArray[3]);
+</script>
+<br><br>
+<script>
+	if (pathArray[3] == 'about') 
+	{
+	document.write("path is about");
+	document.getElementById('a').innerHTML = '<u>About</u>';
+	}
+	if
+	 (pathArray[3] == "web") 
+	{
+ 	document.write("path is web");
+ 	document.getElementById('b').innerHTML = '<u>Web</u>';
+	 }
+	 if (pathArray[3] == "art") {
+ 	document.write("path is art");
+ 	document.getElementById('c').innerHTML = '<u>Art</u>';
+	 };
+	 if (pathArray[3] == "other") {
+ 	document.write("path is other");
+ 	document.getElementById('d').innerHTML = '<u>Other</u>';
+	 };
+</script>
+-->
+<script src="d/js/menu.js"></script>
+<script src="d/js/jquery-2.1.0.min.js"></script>
+<script src="d/js/custom.modernizr.js"></script>
+<script src="d/js/html5shiv.js"></script>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+</body>
+</html>
