dad93cf88f49f6f2c8ab046c184adc5515515490
Timothy Comeau <timothy@timothycomeau.com>
2014-05-15 12:39:25 -0400

    titled index.php page

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>Title</title>
+<title>Menu-Highlight Example</title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="d/css/style.css">
